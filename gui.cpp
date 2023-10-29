73fbd24ab41a1372ee5034d85a0bfeafee5b4b50
Advik <advik.b@gmail.com>
2023-10-29 13:13:52 +0530

    Update gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+#ifdef _DEBUG
+    #include <iostream>
+#endif
 
 // Global variables, used in renderUI()
 ImColor bgColor = ImColor(40, 44, 52);
@@ -10,7 +12,10 @@ void renderUI() {
     ImGui::ColorEdit3("Change background color",(float*)&bgColor,ImGuiColorEditFlags_NoInputs);
     // Add a button that prints a message when clicked
     if (ImGui::Button("Button")) {
+#ifdef _DEBUG
         std::cout << "Button pressed\n";
+#endif
+
     }
     ImGui::End();
 
