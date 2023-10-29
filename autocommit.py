7c9c4ac4c209fb9a839bfc43ba1742c3080a645c
Advik <advik.b@gmail.com>
2023-10-29 12:29:52 +0530

    Create autocommit.py

---
+++ autocommit.py
@@ -0,0 +1,16 @@
+# Makes my commits look like I'm working hard
+import os
+import subprocess
+
+with open('temp.txt', 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    if line.startswith('#'):
+        continue
+    else:
+        line = line.rstrip()
+        subprocess.run(['git', 'add', line], shell=True)
+        subprocess.run(['git', 'commit', '-m', 'Deleted: %s' % line.split('/')[-1], line], shell=True)
+        print('Deleted: %s' % line.split('/')[-1])
+        subprocess.run(['git', 'push', 'origin', 'master'], shell=True)
